412d60c7c85b4e869127a948719553e44e7fb1e5
LasseTN <103989770+LasseTN@users.noreply.github.com>
2024-04-10 15:48:41 +0200

    Genre and Location too

--- Book-REST-Service/Controllers/GenreController.cs
+++ Book-REST-Service/Controllers/GenreController.cs
@@ -15,63 +15,63 @@ namespace Book_REST_Service.Controllers {
         }
 
         // GET: api/<GenreController>
-        //[HttpGet]
-        //public async Task<ActionResult<List<Genre>>> Get() {
-        //    ActionResult<List<Genre>> foundReturn;
-        //    List<Genre>? foundGenres = await _genreControl.GetAll();
-        //    if (foundGenres != null) {
-        //        foundReturn = Ok(foundGenres);
-        //    } else {
-        //        foundGenres = new List<Genre>();
-        //        foundReturn = Ok(foundGenres);
-        //    }
-        //    return foundReturn;
-        //}
+        [HttpGet]
+        public async Task<ActionResult<List<Genre>>> Get() {
+            ActionResult<List<Genre>> foundReturn;
+            List<Genre>? foundGenres = await _genreControl.GetAll();
+            if (foundGenres != null) {
+                foundReturn = Ok(foundGenres);
+            } else {
+                foundGenres = new List<Genre>();
+                foundReturn = Ok(foundGenres);
+            }
+            return foundReturn;
+        }
 
-        //// POST api/<GenreController>
-        //[HttpPost]
-        //public async Task<IActionResult> CreateGenre([FromBody] Genre genreToCreate) {
-        //    IActionResult foundResult;
-        //    int insertedId = await _genreControl.Create(genreToCreate);
-        //    if (insertedId == -1) {
-        //        foundResult = StatusCode(500);
-        //    } else {
-        //        genreToCreate.GenreId = insertedId;
-        //        foundResult = CreatedAtAction(nameof(Get), new {id = insertedId}, genreToCreate);
-        //    }
-        //    return foundResult;
-        //}
+        // POST api/<GenreController>
+        [HttpPost]
+        public async Task<IActionResult> CreateGenre([FromBody] Genre genreToCreate) {
+            IActionResult foundResult;
+            int insertedId = await _genreControl.Create(genreToCreate);
+            if (insertedId == -1) {
+                foundResult = StatusCode(500);
+            } else {
+                genreToCreate.GenreId = insertedId;
+                foundResult = CreatedAtAction(nameof(Get), new { id = insertedId }, genreToCreate);
+            }
+            return foundResult;
+        }
 
-        //// PUT api/<GenreController>/5
-        //[HttpPut("{genreId}")]
-        //public async Task<IActionResult> UpdateGenre(int genreId, [FromBody] Genre genreToUpdate) {
-        //    IActionResult foundResult;
+        // PUT api/<GenreController>/5
+        [HttpPut("{genreId}")]
+        public async Task<IActionResult> UpdateGenre(int genreId, [FromBody] Genre genreToUpdate) {
+            IActionResult foundResult;
 
-        //    bool isUpdated = await _genreControl.Update(genreId, genreToUpdate);
-        //    if (isUpdated) {
-        //        foundResult = Ok();
-        //    } else {
-        //        foundResult = StatusCode(500);
-        //    }
-        //    return foundResult;
-        //}
+            bool isUpdated = await _genreControl.Update(genreId, genreToUpdate);
+            if (isUpdated) {
+                foundResult = Ok();
+            } else {
+                foundResult = StatusCode(500);
+            }
+            return foundResult;
+        }
 
-        //// DELETE api/<GenreController>/5
-        //[HttpDelete("{genreId}")]
-        //public async Task<IActionResult> DeleteGenre(int genreId) {
-        //    IActionResult foundResult;
+        // DELETE api/<GenreController>/5
+        [HttpDelete("{genreId}")]
+        public async Task<IActionResult> DeleteGenre(int genreId) {
+            IActionResult foundResult;
 
-        //    if (genreId > 0) {
-        //        bool isDeleted = await _genreControl.Delete(genreId);
-        //        if (isDeleted) {
-        //            foundResult = NoContent();
-        //        } else {
-        //            foundResult = StatusCode(500);
-        //        }
-        //    } else {
-        //        foundResult = BadRequest();
-        //    }
-        //    return foundResult;
-        //}
+            if (genreId > 0) {
+                bool isDeleted = await _genreControl.Delete(genreId);
+                if (isDeleted) {
+                    foundResult = NoContent();
+                } else {
+                    foundResult = StatusCode(500);
+                }
+            } else {
+                foundResult = BadRequest();
+            }
+            return foundResult;
+        }
     }
 }
